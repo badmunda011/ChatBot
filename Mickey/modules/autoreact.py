08f743dffc7b168d92a23703e08ae643f2e2fb06
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-05-24 19:23:24 +0530

    Update autoreact.py

--- Mickey/modules/autoreact.py
+++ Mickey/modules/autoreact.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 import requests
 import random
+import config
 
 api_id = 26480985 #--Add your Api Id here
 api_hash = '56c935fae1c5c86ba5a3af655f8caa9d' #--Enter Api Hash Here
